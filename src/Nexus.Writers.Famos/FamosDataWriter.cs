78a1b0e2678b9e637532b65404ef8e4db1e049c3
Apollo3zehn <git@m1.apollo3zehn.net>
2021-11-15 18:12:30 +0100

    Initial commit.

---
+++ src/Nexus.Writers.Famos/FamosDataWriter.cs
@@ -0,0 +1,202 @@
+﻿using ImcFamosFile;
+using Nexus.DataModel;
+using Nexus.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nexus.Writers.Famos
+{
+    [DataWriterFormatName("imc FAMOS v2 (*.dat)")]
+    [ExtensionIdentification("Famos", "Famos .dat writer", "Writes data into Famos files.")]
+    public class FamosDataWriter : IDataWriter
+    {
+        #region "Fields"
+
+        private FamosFile _famosFile = null!;
+        private TimeSpan _lastSamplePeriod;
+
+        #endregion
+
+        #region Properties
+
+        private DataWriterContext Context { get; set; } = null!;
+
+        #endregion
+
+        #region "Methods"
+
+        public Task SetContextAsync(
+            DataWriterContext context,
+            CancellationToken cancellationToken)
+        {
+            this.Context = context;
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(
+            DateTime fileBegin, 
+            TimeSpan filePeriod,
+            TimeSpan samplePeriod, 
+            CatalogItem[] catalogItems, 
+            CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                _lastSamplePeriod = samplePeriod;
+
+                var totalLength = filePeriod.Ticks / samplePeriod.Ticks;
+                var dx = samplePeriod.TotalSeconds;
+                var root = this.Context.ResourceLocator.ToPath();
+                var filePath = Path.Combine(root, $"{fileBegin.ToString("yyyy-MM-ddTHH-mm-ss")}Z_{samplePeriod.ToUnitString()}.dat");
+
+                if (File.Exists(filePath))
+                    throw new Exception($"The file {filePath} already exists. Extending an already existing file with additional resources is not supported.");
+
+                var famosFile = new FamosFileHeader();
+
+                // file
+                var metadataGroup = new FamosFileGroup("Metadata");
+
+                metadataGroup.PropertyInfo = new FamosFilePropertyInfo(new List<FamosFileProperty>()
+                {
+                    new FamosFileProperty("system_name", "Nexus"),
+                    new FamosFileProperty("date_time", fileBegin.ToString("yyyy-MM-ddTHH-mm-ss") + "Z"),
+                    new FamosFileProperty("sample_period", samplePeriod.ToUnitString()),
+                });
+
+                famosFile.Groups.Add(metadataGroup);
+
+                var field = new FamosFileField(FamosFileFieldType.MultipleYToSingleEquidistantTime);
+
+                foreach (var catalogItemGroup in catalogItems.GroupBy(catalogItem => catalogItem.Catalog))
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var catalog = catalogItemGroup.Key;
+
+                    // file -> catalog
+                    var catalogGroup = new FamosFileGroup(catalog.Id);
+
+                    catalogGroup.PropertyInfo = new FamosFilePropertyInfo();
+
+                    if (catalog.Properties is not null)
+                    {
+                        foreach (var entry in catalog.Properties)
+                        {
+                            catalogGroup.PropertyInfo.Properties.Add(new FamosFileProperty(entry.Key, entry.Value));
+                        }
+                    }
+
+                    famosFile.Groups.Add(catalogGroup);
+
+                    // for each context group
+                    if (totalLength * (double)FamosDataWriter.SizeOf(NexusDataType.FLOAT64) > 2 * Math.Pow(10, 9))
+                        throw new Exception(ErrorMessage.FamosWriter_DataSizeExceedsLimit);
+
+                    // file -> catalog -> resources
+                    foreach (var catalogItem in catalogItemGroup)
+                    {
+                        var channel = this.PrepareChannel(field, catalogItem, (int)totalLength, fileBegin, dx);
+                        catalogGroup.Channels.Add(channel);
+                    }
+                }
+
+                famosFile.Fields.Add(field);
+
+                //
+                famosFile.Save(filePath, _ => { });
+                _famosFile = FamosFile.OpenEditable(filePath);
+            });
+        }
+
+        public Task WriteAsync(
+            TimeSpan fileOffset,
+            WriteRequest[] requests,
+            IProgress<double> progress,
+            CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                var offset = fileOffset.Ticks / _lastSamplePeriod.Ticks;
+
+                var requestGroups = requests
+                    .GroupBy(request => request.CatalogItem.Catalog)
+                    .ToList();
+
+                var field = _famosFile.Fields.First();
+
+                _famosFile.Edit(writer =>
+                {
+                    foreach (var requestGroup in requestGroups)
+                    {
+                        var catalog = requestGroup.Key;
+                        var requestGroupArray = requestGroup.ToArray();
+
+                        for (int i = 0; i < requestGroupArray.Length; i++)
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+
+                            var component = field.Components[i];
+                            var data = requestGroupArray[i].Data;
+
+                            _famosFile.WriteSingle<double>(writer, component, (int)offset, data.Span);
+                        }
+                    }
+                });
+            });
+        }
+
+        public Task CloseAsync(
+            CancellationToken cancellationToken)
+        {
+            _famosFile.Dispose();
+            return Task.CompletedTask;
+        }
+
+        private FamosFileChannel PrepareChannel(FamosFileField field, CatalogItem catalogItem, int totalLength, DateTime startDateTme, double dx)
+        {
+            // component 
+            var representationName = $"{catalogItem.Resource.Id}_{catalogItem.Representation.Id}";
+
+            var unit = catalogItem.Resource.Properties is null
+                ? string.Empty
+                : catalogItem.Resource.Properties.GetValueOrDefault("Unit", string.Empty);
+
+            var calibration = new FamosFileCalibration(false, 1, 0, false, unit);
+
+            var component = new FamosFileAnalogComponent(representationName, FamosFileDataType.Float64, totalLength, calibration)
+            {
+                XAxisScaling = new FamosFileXAxisScaling((decimal)dx) { Unit = "s" },
+                TriggerTime = new FamosFileTriggerTime(startDateTme, FamosFileTimeMode.Unknown),
+            };
+
+            // attributes
+            var channel = component.Channels.First();
+
+            channel.PropertyInfo = new FamosFilePropertyInfo();
+
+            if (catalogItem.Resource.Properties is not null)
+            {
+                foreach (var entry in catalogItem.Resource.Properties)
+                {
+                    channel.PropertyInfo.Properties.Add(new FamosFileProperty(entry.Key, entry.Value));
+                }
+            }
+
+            field.Components.Add(component);
+
+            return channel;
+        }
+
+        private static int SizeOf(NexusDataType dataType)
+        {
+            return ((ushort)dataType & 0x00FF) / 8;
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Nexus.Writers.Famos.Tests/FamosDataWriterTests.cs
@@ -0,0 +1,123 @@
+using ImcFamosFile;
+using Microsoft.Extensions.Logging.Abstractions;
+using Nexus.DataModel;
+using Nexus.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nexus.Writers.Famos.Tests
+{
+    public class FamosDataWriterTests : IClassFixture<DataWriterFixture>
+    {
+        private DataWriterFixture _fixture;
+
+        public FamosDataWriterTests(DataWriterFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task CanWriteFiles()
+        {
+            var targetFolder = _fixture.GetTargetFolder();
+            var dataWriter = new FamosDataWriter() as IDataWriter;
+
+            var context = new DataWriterContext(
+                ResourceLocator: new Uri(targetFolder),
+                Configuration: new Dictionary<string, string>(),
+                Logger: NullLogger.Instance);
+
+            await dataWriter.SetContextAsync(context, CancellationToken.None);
+
+            var begin = new DateTime(2020, 01, 01, 0, 0, 0, DateTimeKind.Utc);
+            var samplePeriod = TimeSpan.FromSeconds(1);
+
+            var catalogItems = _fixture.Catalogs.SelectMany(catalog => catalog.Resources
+                .SelectMany(resource => resource.Representations.Select(representation => new CatalogItem(catalog, resource, representation))))
+                .ToArray();
+
+            var random = new Random(Seed: 1);
+
+            var length = 1000;
+
+            var data = new[]
+            {
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * 1e4)
+                    .ToArray(),
+
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * -1)
+                    .ToArray(),
+
+                Enumerable
+                    .Range(0, length)
+                    .Select(value => random.NextDouble() * Math.PI)
+                    .ToArray()
+            };
+
+            var requests = catalogItems
+                .Select((catalogItem, i) => new WriteRequest(catalogItem, data[i]))
+                .ToArray();
+
+            await dataWriter.OpenAsync(begin, TimeSpan.FromSeconds(2000), samplePeriod, catalogItems, CancellationToken.None);
+            await dataWriter.WriteAsync(TimeSpan.Zero, requests, new Progress<double>(), CancellationToken.None);
+            await dataWriter.WriteAsync(TimeSpan.FromSeconds(length), requests, new Progress<double>(), CancellationToken.None);
+            await dataWriter.CloseAsync(CancellationToken.None);
+
+            var actualFilePaths = Directory
+                .GetFiles(targetFolder)
+                .OrderBy(value => value)
+                .ToArray();
+
+            // assert
+            Assert.True(actualFilePaths.Length == 1);
+
+            using var famosFile = FamosFile.Open(actualFilePaths.First());
+
+            Assert.True(famosFile.Fields.Count == 1);
+
+            var metadata = famosFile.Groups[0];
+            Assert.Equal("Metadata", metadata.Name);
+
+            // catalog 1
+            var catalog1 = famosFile.Groups[1];
+            Assert.Equal(_fixture.Catalogs[0].Id, catalog1.Name);
+            var representations1 = _fixture.Catalogs[0].Resources.SelectMany(resource => resource.Representations).ToList();
+            Assert.Equal(representations1.Count, catalog1.Channels.Count);
+            var properties1 = catalog1.PropertyInfo.Properties;
+            AssertProperties(_fixture.Catalogs[0].Properties, catalog1.PropertyInfo.Properties);
+            AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Channels[0].PropertyInfo.Properties);
+            AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Channels[1].PropertyInfo.Properties);
+
+            // catalog 2
+            var catalog2 = famosFile.Groups[2];
+            Assert.Equal(_fixture.Catalogs[1].Id, catalog2.Name);
+            var representations2 = _fixture.Catalogs[1].Resources.SelectMany(resource => resource.Representations).ToList();
+            Assert.Equal(representations2.Count, catalog2.Channels.Count);
+            AssertProperties(_fixture.Catalogs[1].Properties, catalog2.PropertyInfo.Properties);
+            AssertProperties(_fixture.Catalogs[1].Resources[0].Properties, catalog2.Channels[0].PropertyInfo.Properties);
+
+            void AssertProperties(IReadOnlyDictionary<string, string> expected, List<FamosFileProperty> actual)
+            {
+                Assert.Equal(expected.Count, actual.Count);
+
+                for (int i = 0; i < actual.Count; i++)
+                {
+                    var key = actual[i].Name;
+                    var value = actual[i].Value;
+
+                    var match = expected[key];
+                    Assert.Equal(match, value);
+                }
+            }
+        }
+    }
+}
